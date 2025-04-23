02e0f0df61bc77257493bb24ade69de981aaf86d
sswck <sswcg@daum.net>
2025-04-23 10:20:54 +0900

    REFACTOR: 파일 정리
    최종 프로젝트 구조 확립

--- sidebar.js
+++
@@ -1,9 +0,0 @@
-function handleSidebarClick() {
-  // Add your onclick functionality here
-  alert("Sidebar item clicked");
-}
-
-function openSubscribedChannelPage() {
-  // Add your onclick functionality here
-  alert("Subscribed channel page opened");
-}
