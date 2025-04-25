83d9efac2b3ff138efc4a5d2bb6b79905657f5a0
eastdh <eastdh@gmail.com>
2025-04-25 10:30:29 +0900

    FIX: try-catch 추가

--- scripts/video.js
+++ scripts/video.js
@@ -14,25 +14,24 @@ async function init() {
   }
 
   // API 호출하여 동영상 정보, 채널 정보, 동영상 목록 가져오기
-  const videoData = await getVideoInfo(videoID);
-  if (!videoData) {
-    console.error("Failed to fetch video data.");
-    return;
+  try {
+    const videoData = await getVideoInfo(videoID);
+    displayVideoInfo(videoData); // 동영상 정보 표시
+  } catch (error) {
+    console.error("Error fetching video info:", error);
   }
-  const channelData = await getChannelInfo(videoData.channel_id);
-  if (!channelData) {
-    console.error("Failed to fetch channel data.");
-    return;
+  try {
+    const channelData = await getChannelInfo(videoData.channel_id);
+    displayChannelInfo(channelData); // 채널 정보 표시
+  } catch (error) {
+    console.error("Error fetching channel info:", error);
   }
-  const videoListData = await getVideoList();
-  if (!videoListData) {
-    console.error("Failed to fetch video list data.");
-    return;
+  try {
+    const videoListData = await getVideoList();
+    displayVideoList(videoListData); // 동영상 목록 표시
+  } catch (error) {
+    console.error("Error fetching video list:", error);
   }
-
-  displayVideoInfo(videoData); // 동영상 정보 표시
-  displayChannelInfo(channelData); // 채널 정보 표시
-  displayVideoList(videoListData); // 동영상 목록 표시
 }
 
 function displayVideoInfo(data) {
