e6bf8d45798c2e892873527f8e1f97e5628a87ab
eastdh <eastdh@gmail.com>
2025-05-07 14:36:20 +0900

    FEAT: 사이드바에 현재 페이지 표시 기능 추가

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -6,16 +6,27 @@ function initSidebar() {
 }
 
 function initButtons() {
-  document.querySelector("#button-home").addEventListener("click", function () {
-    window.location.href = "/index.html";
-  });
-
-  document.querySelector("#button-subscriptions").addEventListener("click", function () {
-    window.location.href = "/index.html?subscriptions=true";
-  });
-
-  document.querySelector("#button-liked").addEventListener("click", function () {
-    window.location.href = "/index.html?liked=true";
+  const urlParams = new URLSearchParams(window.location.search);
+  const sidebarButtons = document.querySelectorAll(".sidebar-item");
+  const currentPage = window.location.search;
+  sidebarButtons.forEach((button) => {
+    button.classList.remove("selected"); // 모든 버튼에서 selected 클래스 제거
+    const targetPage = button.getAttribute("data-target-page");
+    if (targetPage && targetPage.includes(currentPage) && currentPage.length > 0) {
+      // Check if the button's target page matches the current page
+      button.classList.add("selected"); // Add the "selected" class if it matches
+    } else if (currentPage.length === 0) {
+      // If no query parameters, set the first button as selected
+      const firstButton = sidebarButtons[0];
+      if (firstButton) {
+        firstButton.classList.add("selected");
+      }
+    }
+    button.addEventListener("click", function () {
+      if (targetPage) {
+        window.location.href = targetPage;
+      }
+    });
   });
 }
 
