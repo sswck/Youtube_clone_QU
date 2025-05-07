4e380635c7b97233de38e3549703c6c2371a18b8
eastdh <eastdh@gmail.com>
2025-05-07 11:38:05 +0900

    FEAT: 사이드바 버튼 기능 추가

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,6 +2,21 @@ import { renderSubscriptions } from "./subscription.js";
 
 function initSidebar() {
   renderSubscriptions(); // 구독 목록 렌더링
+  initButtons();
+}
+
+function initButtons() {
+  document.querySelector("#button-home").addEventListener("click", function () {
+    window.location.href = "/index.html";
+  });
+
+  document.querySelector("#button-subscriptions").addEventListener("click", function () {
+    window.location.href = "/index.html?subscriptions=true";
+  });
+
+  document.querySelector("#button-liked").addEventListener("click", function () {
+    window.location.href = "/index.html?liked=true";
+  });
 }
 
 // 0.1초마다 sidebar 등장 여부 확인
